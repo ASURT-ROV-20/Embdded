92d471d1e4d033bec835bc6a51a220611b44f8ff
mgtm <m.g.t.m.prog@gmail.com>
2020-02-22 16:51:51 +0200

    Testing SerialEthernet

--- core/avrServer.py
+++
@@ -1,4 +0,0 @@
-from machine import UART
-
-def echoTest():
-    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from core.adcMesure import adcMesure, init_ADC
 from core.pwmMesure import pwmMesure
+from core.serialEthernet import SerialEthernet
 from machine import RTC
 import ujson as json
 import gc
@@ -7,16 +8,22 @@ import time
 
 realTime = RTC()
 # init_ADC()
+ether = SerialEthernet(2,None)
+ether.send("System Started")
 
 while(True):
     data = {}
     data["Time"] = realTime.datetime()
     data["PWM"]  = pwmMesure()
     data["ADC"]  = adcMesure()
+    data = json.dumps(data)
     log = open("sd/log1.txt", 'a')
-    log.write(json.dumps(data))
+    log.write(data)
     log.write('\n')
     log.close()
+    print(data[0:59])
+    ether.send()
     gc.collect()
     print("LOG IS WRITTEN", gc.mem_free())
+    time.sleep(2)
     
